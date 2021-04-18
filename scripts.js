0ad4fe098d91a1e95ba19c11df6ad179b7483565
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-18 17:23:13 +0300

    Code update and styling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,34 @@
-$(document).ready(function(){
-    $('.development img').click(function(){
-        $('.development-show').toggle();
+$(document).ready(function() {
+    $(".d-image").click(function() {
+        $(".d-image").hide();
+        $(".design").toggle();
+
     });
+
+    $(".design").click(function() {
+        $(".design").hide();
+        $(".d-image").toggle();
+
+    });
+
+    $(".dev-image").click(function() {
+        $(".dev-image").hide();
+        $(".development").toggle();
+    });
+
+    $(".development").click(function() {
+        $(".development").hide();
+        $(".dev-image").toggle();
+    });
+
+    $(".p-image").click(function() {
+        $(".p-image").hide();
+        $(".product").toggle();
+    });
+
+    $(".product").click(function() {
+        $(".product").hide();
+        $(".p-image").toggle();
+    });
+
 });
